7ea99ab6661b434fb1368691ea9446f490218eb5
ivanange <ivanangethe1@gmail.com>
2019-09-24 20:03:22 +0100

    test heroku deployement

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: php ws_server.php
---
+++ websocket.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang='en'>
+<head>
+<meta charset='utf-8'>
+<title>Websocket Example</title>
+<link rel='stylesheet' href='websocket.css' media = "all">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+
+<div id='output'></div>
+<form action='submit'>
+<input type ='text' id='textarea'  name='message' action='submit'>
+<input name='submit' type="button" id='input' action='submit'>
+</form>
+<script src='websocket.js'></script>
+</body>
+</html>
