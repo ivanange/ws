e24cf69b90be77ebfe78de56876a337186ea1743
ivanange <ivanangethe1@gmail.com>
2019-09-24 19:21:17 +0100

    test heroku deployement

---
+++ websocket.js
@@ -0,0 +1,60 @@
+URL = 'ws://websocket:4444';
+//URL = 'wss://echo.websocket.org/';
+outputDiv = document.getElementById('output');
+form = document.forms[0];
+connection = new WebSocket(URL);
+
+connection.addEventListener('open', () => {
+    output('CONNECTED', true);
+}, false);
+
+function output(message, server) {
+    var para = document.createElement('p');
+    var clear = document.createElement('p');
+    para.innerHTML = message;
+    attrib = server ? 'server' : 'client';
+    para.setAttribute('class', attrib);
+    para.classList.add('message');
+    clear.setAttribute('id', 'clear');
+    outputDiv.appendChild(para);
+    if (!server) outputDiv.appendChild(clear);
+}
+
+form.submit.addEventListener('click', message, false);
+form.addEventListener('submit', message, false);
+
+function message(event) {
+    event.preventDefault();
+    const text = form.message.value;
+    form.message.value = "";
+    output(text, false);
+    connection.send(text);
+}
+
+connection.addEventListener('close',
+    () => {
+        output('DISCONNECTED', true);
+    }, false);
+
+connection.addEventListener('error',
+    (event) => {
+        output(`<span style='color: red;'>ERROR:${event.data}</span>`, true);
+    }, false);
+
+connection.addEventListener('message',
+    (event) => {
+        output(event.data, true);
+    }, false);
+
+if (window.Notification) {
+    Notification.requestPermission().then((permission) => {
+        if (permission == 'granted') {
+            Notification.permission = permission;
+            console.log(Notification.permission);
+            notification = new Notification('Echo messaging using websocket', {
+                body: 'try sending some mesaages and watch how its sent back to you',
+                icon: 'Flickr-1.4s-200px.gif'
+            });
+        }
+    });
+}
---
+++ ws_server.php
@@ -0,0 +1,44 @@
+#!/php -q
+<?php  
+// Run from command prompt > php -q ws_server.php
+include "phpwebsocket.php";
+
+$server_ip="websocket";  //what is the IP of your server
+
+// Extended basic WebSocket as ws_server
+class ws_server extends phpWebSocket{
+
+
+  //Overridden process function from websocket.class.php
+  function process($user,$msg){
+    $c=0;
+  	$this->say("(user: ".$user->id.") msg> ".$msg);
+    //$this->say("< ".$msg);
+	
+  foreach($this->users as $u) { $this->send($u->socket, $msg);}   
+    
+  }
+  
+ function getTemp()
+  {
+	$jsonurl = "http://api.openweathermap.org/data/2.5/weather?q=New_York,us";
+	$json = file_get_contents($jsonurl);
+	$weather = json_decode($json);
+	$temp = $weather->main->temp;
+
+ if ( !is_numeric($temp) ) 
+	{ 
+	return false; 
+	}
+	else
+	{
+    $temp_f=round((($temp - 273.15) * 1.8) + 32);
+	$temp_celcius=round(($temp - 273.15));
+	return $temp_f."f";
+	}
+ }  //end get Temp
+
+  
+}  //end class
+
+$master = new ws_server($server_ip,4444);
